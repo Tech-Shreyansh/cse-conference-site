114d22ed3b6fc61b61daae72c5cf3dda27a9751d
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2024-02-17 12:45:36 +0530

    enhanced responsiveness

--- src/app/travel-grant/page.js
+++ src/app/travel-grant/page.js
@@ -7,7 +7,7 @@ const page = () => {
         <div>
         <div>
             <Header active="travel-grant" />
-            <div id="call" className="target mb-5 foothandle">
+            <div id="call" className="target md:mb-5 foothandle">
                 <div className="container" >
                 <h1 className="text-blue-800 text-4xl mb-4">Travel Grant</h1>
                 <b className="text-lg">Travel grant will be provided to IEEE students on first come first serve basis(as per availability of grant).
