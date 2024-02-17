505744a6bd503d661d22ca8f82362f1bf79c023a
Abhinandan <abhinandnn21@gmail.com>
2024-02-17 16:31:33 +0530

    enhanced responsiveness and fixed minor bugs

--- src/app/accomodation/page.js
+++ src/app/accomodation/page.js
@@ -7,10 +7,10 @@ const page = () => {
         <>
         <div>
             <Header active="accomodation" />
-            <div id="call" className="target mb-5 foothandle">
+            <div className="target mb-5 px-[5%] foothandle">
                 <div className="container" >
-                <h1 className="text-blue-800 text-4xl mb-4">Accomodation</h1>
-                    <p className="text-[1.125rem]">Participants will make their own arrangements for travel, boarding, lodging etc. However, limited accommodation facility may be provided to the participants in hostels and guest rooms of the institute on special request. For lady participants’ accommodation shall be arranged in the Girls hostels of the institute, on request. For guests, the accommodation shall be arranged by our Institute.
+                <h1 className="text-blue-800 sm:text-4xl text-2xl mb-4">Accomodation</h1>
+                    <p className="sm:text-[1.125rem] text-[1rem]">Participants will make their own arrangements for travel, boarding, lodging etc. However, limited accommodation facility may be provided to the participants in hostels and guest rooms of the institute on special request. For lady participants’ accommodation shall be arranged in the Girls hostels of the institute, on request. For guests, the accommodation shall be arranged by our Institute.
                     </p>
                 </div>
             </div>
--- src/app/travel-grant/page.js
+++ src/app/travel-grant/page.js
@@ -5,15 +5,13 @@ import Footer from "../../components/footer"
 const page = () => {
     return (
         <div>
-        <div>
             <Header active="travel-grant" />
-            <div id="call" className="target md:mb-5 foothandle">
+            <div className="target md:mb-5 foothandle px-[5%]">
                 <div className="container" >
-                <h1 className="text-blue-800 text-4xl mb-4">Travel Grant</h1>
-                <b className="text-lg">Travel grant will be provided to IEEE students on first come first serve basis(as per availability of grant).
+                <h1 className="text-blue-800 sm:text-4xl text-2xl mb-4">Travel Grant</h1>
+                <b className="sm:text-lg text-md">Travel grant will be provided to IEEE students on first come first serve basis(as per availability of grant).
                 </b></div>
             </div>
-        </div>
         <div className="absolute left-0 right-0 md:bottom-0">
         <Footer />
     </div>
