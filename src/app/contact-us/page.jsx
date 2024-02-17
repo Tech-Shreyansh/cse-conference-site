0b66397a2f3db713bdd2cd42a197bbc3fdbd5f4f
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2024-02-17 12:54:22 +0530

    fixes

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -8,19 +8,19 @@ const ContactUs = () => {
         <div>
             <Header active='contact-us' />
             <div className="px-[5%] md:h-auto mb-28 md:mb-16">
-                <h1 className="mt-3 mb-3 text-[#1E40AF] text-center underline">Contact Us</h1>
-                <p className="text-[24px] sm:text-[36px]">For conference relatied queries write to us on :-<br />
+                <h1 className="mt-3 text-blue-800 text-2xl md:text-4xl mb-4">Contact Us</h1>
+                <p className="md:text-2xl text-lg font-semibold">For conference relatied queries write to us on :-<br />
                 </p>
-                <ul className=" text-[15px] sm:text-[18px]  pl-0">
+                <ul className="text-[15px] sm:text-[18px] pl-0">
                     <li>
-                        <a href="mailto:acet@akgec.ac.in">acet@akgec.ac.in</a>
+                        <a  href="mailto:acet@akgec.ac.in">acet@akgec.ac.in</a>
                     </li>
                     <li>
                         <a href="mailto:prasadrajesh@akgec.ac.in">prasadrajesh@akgec.ac.in</a>
                     </li>
                 </ul>
-                <p style={{ fontWeight: "400", margin: 0 }} className="text-[24px] sm:text-[36px]">Address:-</p>
-                <p className=" text-[15px] sm:text-[18px]">27th Km Milestone,
+                <p className="md:text-2xl text-lg font-semibold mt-12">Address:-</p>
+                <p className="text-[15px] sm:text-[18px]">27th Km Milestone,
                     Delhi-Meerut Expressway,
                     P.O. Adhyatmik Nagar,
                     Ghaziabad - 201009</p>
