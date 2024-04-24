3d6e266395adf91e742172aa0999970a24ec4fa3
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2024-04-24 20:34:12 +0530

    updated footer size

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,10 @@ import "../app/globals.css"
 import "bootstrap/dist/css/bootstrap.min.css"; 
 const Footer = () => {
   return (
-    <div id="footer"  className="absolute bg-[#1f1f1f] xl:bg-white w-full mt-[30px] d-flex flex-column align-items-center justify-content-center pb-0 mb-0" >
-    <img className="mt-5 w-[4rem] bg-white rounded-full" src="akgec.png" />
-    <p className='md:text-lg text-gray-400 md:text-xl mt-4 w-3/5 text-center'>Venue: Department of Computer Science & Engineering</p>
-    <p className="mt-2 mb-4 text-gray-500 text-lg text-center md:text-2xl">Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+    <div id="footer"  className="absolute bg-[#1f1f1f] xl:bg-white w-full mt-[30px] d-flex flex-column align-items-center justify-content-center pb-0 mb-0 text-xs pt-2" >
+    <img className="w-[4rem] bg-white rounded-full md:mt-2" src="akgec.png" />
+    <p className='text-gray-400 text-base md:text-lg mt-2 md:mt-4 md:w-3/5 text-center'>Venue: Department of Computer Science & Engineering</p>
+    <p className="my-2 md:mb-4 text-gray-500 text-center text-base md:text-lg w-11/12">Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
     <div class="row text-light">
         <div class="col-sm"></div>
     </div>
